9515e6aada3a1b9f563dea418542526787050c06
angryduck08 <gkaeogus7@gmail.com>
2022-11-25 15:14:15 +0900

    Feat: conversion started 22.11.25

---
+++ src/main/java/com/groupware/tetris/controller/attendance/HrController.java
@@ -0,0 +1,8 @@
+package com.groupware.tetris.controller.attendance;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HrController {
+
+}
---
+++ src/main/java/com/groupware/tetris/entity/attendance/Hr.java
@@ -0,0 +1,10 @@
+package com.groupware.tetris.entity.attendance;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Hr")
+public class Hr {
+
+}
