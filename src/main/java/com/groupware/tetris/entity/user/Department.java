3966f3d31ca34e73e9f4146e95587bda8aeb21ef
chulbin jung <chulbin1890@naver.com>
2022-11-28 09:25:10 +0900

    Feat: 22.11.28 entity 일부, register폼 수정 및 DTO추가

--- src/main/java/com/groupware/tetris/entity/user/Department.java
+++ src/main/java/com/groupware/tetris/entity/user/Department.java
@@ -1,9 +1,15 @@
 package com.groupware.tetris.entity.user;
 
+import com.groupware.tetris.dto.user.DepartmentDto;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 @Table(name="department")
 
@@ -19,4 +25,12 @@ public class Department{
     @OneToMany(mappedBy = "department", cascade = CascadeType.ALL,
             orphanRemoval = true,fetch = FetchType.LAZY)
     List<Employee> employees = new ArrayList<>();
+
+    public static Department createDepartment(DepartmentDto departmentDto){
+        Department department = new Department();
+        department.setId(departmentDto.getId());
+        department.setName(departmentDto.getName());
+        department.setHead(departmentDto.getHead());
+        return department;
+    }
 }
--- src/main/java/com/groupware/tetris/entity/user/Employee.java
+++ src/main/java/com/groupware/tetris/entity/user/Employee.java
@@ -1,8 +1,22 @@
 package com.groupware.tetris.entity.user;
 
-import javax.persistence.*;
-import java.sql.Timestamp;
+import com.groupware.tetris.constant.Role;
+import com.groupware.tetris.dto.user.DepartmentDto;
+import com.groupware.tetris.dto.user.EmployeeFormDto;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.persistence.*;
+import javax.validation.constraints.Null;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Setter
+@Getter
+@ToString
 @Entity
 @Table(name = "employee")
 
@@ -12,19 +26,36 @@ public class Employee {
     @GeneratedValue(strategy =GenerationType.AUTO)
     private Long id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String password;
     private String phoneNumber;
-    private Timestamp hireddate;
-    private Timestamp resigndate;
+    private LocalDateTime hireddate;
+    private LocalDateTime resigndate;
     private Integer totalVac;
     private Integer useVac;
     private String birth;
     private String position;
     private boolean Enabled;
-
+        /*private Long d_Id;*/
+    @Enumerated(EnumType.STRING)
+    private Role role;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "d_id")
     private Department department;
 
+    public static Employee createEmployee(EmployeeFormDto employeeFormDto, DepartmentDto departmentDto, PasswordEncoder passwordEncoder){
+        Employee employee = new Employee();
+        employee.setName(employeeFormDto.getName());
+        employee.setEmail(employeeFormDto.getEmail());
+        employee.setPassword(employeeFormDto.getPassword());
+        employee.setPhoneNumber(employeeFormDto.getPhoneNumber());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        employee.setHireddate(LocalDateTime.parse(employeeFormDto.getHireddate(),formatter));
+        employee.setResigndate(LocalDateTime.parse(employeeFormDto.getResigndate(),formatter));
+        employee.setBirth(employeeFormDto.getBirth());
+        //employee.setD_Id(departmentDto.getId());
+        employee.setRole(Role.ADMIN);
+        return employee;
+    }
 }
