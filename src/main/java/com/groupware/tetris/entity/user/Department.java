239169968b96a6042abb6f09ea35a56951dc17e2
eojin kim <sub05073@naver.com>
2022-11-29 17:09:08 +0900

    Feat: 22.11.29 캘린더 CRUD, 조직도 정보 조회

--- src/main/java/com/groupware/tetris/entity/user/Department.java
+++ src/main/java/com/groupware/tetris/entity/user/Department.java
@@ -1,12 +1,16 @@
 package com.groupware.tetris.entity.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name="department")
-
+@Getter
+@Setter
 public class Department{
     @Id
     @Column(name = "d_id")
