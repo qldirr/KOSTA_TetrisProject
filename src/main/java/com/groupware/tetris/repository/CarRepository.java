262be35d13328596eecc70d74b6cd4ef8f184a5f
Joon <ckjoon89@gmail.com>
2022-11-30 09:44:43 +0900

    Chore: 22.11.30 lhy branch merge

--- src/main/java/com/groupware/tetris/repository/CarRepository.java
+++ src/main/java/com/groupware/tetris/repository/CarRepository.java
@@ -5,9 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CarRepository extends JpaRepository<Car, Long> {
+public interface CarRepository extends JpaRepository<Car, Long>, CarRepositoryCustom {
 
-    List<Car> findBy(String modelNm);
+    List<Car> findByModelNm(String modelNm);
 
 
 }
