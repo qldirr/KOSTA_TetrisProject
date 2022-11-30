605e171ebc107ea6e0d4466f792ca2e3b5c188fd
Joon <ckjoon89@gmail.com>
2022-11-30 09:46:14 +0900

    Chore: 22.11.30 lhy branch merge

--- src/main/java/com/groupware/tetris/repository/CarRepositoryCustom.java
+++ src/main/java/com/groupware/tetris/repository/CarRepositoryCustom.java
@@ -7,5 +7,5 @@ import org.springframework.data.domain.Pageable;
 
 public interface CarRepositoryCustom {
 
-    Page<Car> getCarListPage(CarFormDto carFormDto,Pageable pageable);
+//    Page<Car> getCarListPage(CarFormDto carFormDto,Pageable pageable);
 }
