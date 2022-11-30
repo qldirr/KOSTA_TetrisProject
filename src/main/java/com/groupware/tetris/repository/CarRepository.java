99db84457b73f8a2f5d6107748db9b287510a91e
hwayoung lee <hylee13ee@gmail.com>
2022-11-30 10:29:41 +0900

    Feat: 22.11.30 차량예약도전

--- src/main/java/com/groupware/tetris/repository/CarRepository.java
+++ src/main/java/com/groupware/tetris/repository/CarRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
 
-    List<Car> findBy(String modelNm);
+    List<Car> findByModelNm(String modelNm);
 
 
 }
